aa9d789eb337b56f63e4afd3b26112c1fa4cccbc
lregebro <devnull@localhost>
2004-04-02 14:56:11 +0000

    Adding and deleting fields now work.

--- skins/cps_typemaker/cpstypes_add_field.py
+++ skins/cps_typemaker/cpstypes_add_field.py
@@ -7,7 +7,7 @@ kw = {'fields': [new_field],}
 
 layout.manage_addCPSWidget(new_field, 'String Widget', **kw)
 layoutdef = layout.getLayoutDefinition()
-layoutdef['rows'].append([{'ncols': 1, 'widget_id': 'w__'+new_field}])
+layoutdef['rows'].append([{'ncols': 1, 'widget_id': new_field}])
 layout.setLayoutDefinition(layoutdef)
 
 return context.cpstypes_view(type_id=layout_id)
--- skins/cps_typemaker/cpstypes_modify.py
+++ skins/cps_typemaker/cpstypes_modify.py
@@ -14,19 +14,20 @@ layout = context.portal_layouts[layoutid]
     
 if action == 'delete':
     selected = REQUEST.get('selected_ids', [])
+    widgetnames = [layout[w].getWidgetId() for w in selected]
+        
     layout.manage_delObjects(selected)
     layoutdef = layout.getLayoutDefinition()
     new_rows = []
     for row in layoutdef['rows']:
         new_widgets = []
         for widget in row:
-            if widget['widget_id'] != layoutid:
+            if widget['widget_id'] not in widgetnames:
                 new_widgets.append(widget)
         if new_widgets:
             new_rows.append(row)
     layoutdef['rows'] = new_rows
     layout.setLayoutDefinition(layoutdef)
-    raise str(layoutdef)
 
     
 return RESPONSE.redirect('cpstypes_view?type_id='+layoutid)
