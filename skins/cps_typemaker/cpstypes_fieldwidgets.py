1a17da1c51f16cc586f3c0586502e97198a25b1e
lregebro <devnull@localhost>
2004-04-05 16:28:16 +0000

    Skins to manage the schema

---
+++ skins/cps_typemaker/cpstypes_fieldwidgets.py
@@ -0,0 +1,11 @@
+##parameters=field_type=None
+
+mapping = { 'CPS String List Field': ('MultiSelect Widget',),
+            'CPS String Field': ('Text Widget', 'Textarea Widget', 'String Widget',),
+            'CPS DateTime Field': ('DateTime Widget', 'Date Widget',),
+          }
+
+if field_type:
+    return mapping[field_type]
+
+return mapping.keys()
