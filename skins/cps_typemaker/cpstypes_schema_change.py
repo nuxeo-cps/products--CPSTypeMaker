6b5b03caee32e04e537fd04bde2260646850126c
lregebro <devnull@localhost>
2004-04-06 16:26:58 +0000

    Now you can delete fields.

--- skins/cps_typemaker/cpstypes_schema_change.py
+++ skins/cps_typemaker/cpstypes_schema_change.py
@@ -1,13 +1,26 @@
 ##parameters=fieldinfo, REQUEST, RESPONSE=None
 
+action = None
+for key in REQUEST.keys():
+    if key.startswith('action_'):
+        action = key[7:] # remove the 'action_'-part
+        break
+       
+if not action:
+    raise ValueError('No action specified')
+
 schema = context.portal_schemas.metadata
 
 for info in fieldinfo:
     field = schema[info['id']]
-    if field.hasProperty('default_widget'):
-        field.manage_changeProperties(default_widget = info['widget'])
+    is_selected = info.get('is_selected',0)
+    if action == 'delete' and is_selected:    
+        schema.manage_delObjects([info['id']])
     else:
-        field.manage_addProperty('default_widget', info['widget'], 'string')
+        if field.hasProperty('default_widget'):
+            field.manage_changeProperties(default_widget = info['widget'])
+        else:
+            field.manage_addProperty('default_widget', info['widget'], 'string')
 
 if RESPONSE:
     return RESPONSE.redirect(context.portal_url() + '/cpstypes_schema_edit')
