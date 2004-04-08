a543c623a5302f0eb871e45531faf9d361cf979e
lregebro <devnull@localhost>
2004-04-08 13:10:13 +0000

    Lists allowed icon selections.

---
+++ skins/cps_typemaker/cpstypes_get_icons.py
@@ -0,0 +1,11 @@
+return ['dummy_icon.gif',
+        'attachedfile_icon.gif',
+        'book_icon.gif',
+        'event_icon.gif',
+        'flexible_icon.gif',
+        'glossary_icon.gif',
+        'image_icon.gif',
+        'imgallery_icon.gif',
+        'news_icon.gif',
+       ]
+        
