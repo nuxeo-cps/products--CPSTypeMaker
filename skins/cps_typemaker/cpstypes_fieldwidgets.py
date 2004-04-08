81499ec4092061d341009205a67a8c79fffb4613
lregebro <devnull@localhost>
2004-04-08 15:02:15 +0000

    Added the rest of the field and widget types.

--- skins/cps_typemaker/cpstypes_fieldwidgets.py
+++ skins/cps_typemaker/cpstypes_fieldwidgets.py
@@ -1,8 +1,17 @@
 ##parameters=field_type=None
 
-mapping = { 'CPS String List Field': ('MultiSelect Widget',),
-            'CPS String Field': ('Text Widget', 'TextArea Widget', 'String Widget',),
-            'CPS DateTime Field': ('DateTime Widget', 'Date Widget',),
+mapping = { 'CPS String List Field': ('MultiSelect Widget', 'Lines Widget', 
+                'Ordered List Widget', 'Unordered List Widget'),
+            'CPS String Field': ('String Widget', 'TextArea Widget', 
+                'Email Widget', 'Heading Widget', 'Rich Text Editor Widget',
+                'InternalLinks Widget', 'Link Widget', 'Html Widget'),
+            'CPS DateTime Field': ('DateTime Widget', 'Date Widget'),
+            'CPS Password Field': ('Password Widget',),
+            'CPS Int Field': ('Int Widget', 'CheckBox Widget'),
+            'CPS Long Field': ('Long Widget',),
+            'CPS Float Field': ('Float Widget',),
+            'CPS File Field': ('AttachedFile Widget', ' File Widget'),
+            'CPS Image Field': ('Image Widget', 'Photo Widget'),
           }
 
 if field_type:
