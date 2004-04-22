f4a0f2197ee9510f7a6fef01789b3b830c4b18f6
lregebro <devnull@localhost>
2004-04-22 13:24:20 +0000

    More spring cleaning! Sweep, sweep, sweep all that old code away!

--- skins/cps_typemaker/cpstypes_get_schema.py
+++
@@ -1,9 +0,0 @@
-##parameters=
-
-schema_id  = context.cpstypes_get_definitions()['schemaid']
-stool = context.portal_schemas
-if hasattr(stool, schema_id):
-    return getattr(stool, schema_id)
-    
-schema = stool.manage_addCPSSchema(schema_id)
-return schema
